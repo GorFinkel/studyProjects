e1ace254d130fc80e81ca4518d2b59d2abc9397a
GorFinkel <97066710+GorFinkel@users.noreply.github.com>
2022-12-20 01:57:30 +0200

    Create another_Rock_Paper_Scissors.js

---
+++ another_Rock_Paper_Scissors.js
@@ -0,0 +1,52 @@
+console.log('hi');
+
+// let userInput=document.querySelector('.userInput').toLowerCase()
+let potentialChoise=["rock","paper","scissors"]
+let getComputerChoice=potentialChoise[Math.floor(Math.random()*3)]
+console.log(`The PC has chosen: ${getComputerChoice}`);
+
+const getUserChoice=(userInput)=>{
+  userInput=userInput.toLowerCase()
+  if (userInput==='cheatcode'){
+    console.log('user has chosen to use cheats')
+    return userInput
+  }else{
+  if (userInput==='rock' || userInput ==='paper' || userInput ==='scissors') {
+    console.log(`user has chosen ${userInput}`)
+    return userInput
+  }else{console.log('wrong input')}
+}}
+
+let determineWinner=(userChoice,computerChoice)=>{
+   if (userChoice==='cheatcode'){
+  return "User has won using cheats"
+  }if (userChoice===computerChoice){
+  return 'There is a tie'
+}else if(userChoice==='rock'){
+  if (computerChoice==='paper'){
+    return 'PC has won'
+  }else if(computerChoice==='scissors'){
+    return 'User has won'
+ }
+}else if(userChoice==='scissors'){
+  if (computerChoice==='paper'){
+  return 'User has won'
+}else if (computerChoice==='rock'){return 'PC has won'
+}
+}else if(userChoice==='paper'){
+  if(computerChoice==='rock'){
+    return 'User has won'
+  }else if(computerChoice==='scissors'){
+    return 'PC has won'
+  }
+}
+}
+
+
+let userChoice=getUserChoice('cheatcode')
+
+let playGame=()=>{
+  
+console.log(determineWinner(userChoice,getComputerChoice))
+}
+playGame()
